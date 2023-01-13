3151f258eb4e736859a1812b59807ef15a8abf2f
Vitalii Mishchenko <vitalii.mishchenko@mongodb.com>
2023-01-12 18:49:35 -0800

    add few Design definition examples

---
+++ src/designDefinition/examples/basic-design-definition.ts
@@ -0,0 +1,32 @@
+import { DesignDefinition } from '../types/designDefinition';
+
+export const basicDesignDefinition: DesignDefinition = {
+  "searchIndex": {
+    "name": "facets",
+    "databaseName": "sample_airbnb",
+    "collectionName": "listingsAndReviews",
+  },
+  "pipeline": [
+    {
+      "$search": {
+        "text": {
+          "query": "$$SEARCH_QUERY",
+          "path": {
+            "wildcard": "*",
+          },
+        },
+      },
+    },
+    {
+      "$limit": 10,
+    },
+  ],
+  "ui": {
+    "docFieldNamesToRender": [],
+    "docTitleFieldName": "",
+    "url": {
+      "docFieldName": "",
+      "template": "",
+    },
+  },
+};
---
+++ src/designDefinition/examples/compound-design-definition.ts
@@ -0,0 +1,38 @@
+import { DesignDefinition } from '../types/designDefinition';
+
+export const compoundDesignDefinition: DesignDefinition = {
+  "searchIndex": {
+    "name": "facets",
+    "databaseName": "sample_airbnb",
+    "collectionName": "listingsAndReviews",
+  },
+  "pipeline": [
+    {
+      "$search": {
+        "compound": {
+          "should": [
+            {
+              "text": {
+                "query": "$$SEARCH_QUERY",
+                "path": {
+                  "wildcard": "*",
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    {
+      "$limit": 10,
+    },
+  ],
+  "ui": {
+    "docFieldNamesToRender": [],
+    "docTitleFieldName": "",
+    "url": {
+      "docFieldName": "",
+      "template": "",
+    },
+  },
+};
